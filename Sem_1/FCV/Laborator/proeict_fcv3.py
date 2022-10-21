b044578e401f259052c90f0f0bd56de9a59f8aad
Ancuta Catalin-Mihai <ancuta.catalin.mihai@gmail.com>
2022-10-21 13:57:10 +0300

    week 4 implementation partialy done

--- Sem_1/FCV/Laborator/proeict_fcv3.py
+++ Sem_1/FCV/Laborator/proeict_fcv3.py
@@ -33,9 +33,60 @@ def edgeDetection(image: cv.Mat):
     cv.imshow('Edge Detection', edgeDetectionImage)
     cv.waitKey()
 
+def translation(image: cv.Mat, newX: int, newY: int):
+    #get the height, width and channels of the image
+    height, width, channels = image.shape
+    #define the translation matrix
+    # shape should be of form:
+    translationMatrix = np.array([[1, 0, newX], [0, 1, newY]])
+    translatedImage = np.zeros(image.shape)
+
+    #compute new values
+    for i in range(height - newY):
+        for j in range(width - newX):
+            for k in range(channels):
+                origin = np.array([i, j, 1])
+                translation_values = np.dot(translationMatrix, origin)
+
+                if translation_values[0] >= 0 and translation_values[0] < width and translation_values[1] >= 0 and translation_values[1] < height:
+                    translatedImage[translation_values[0], translation_values[1]] = image[i, j]
+    cv.imshow('Translated image', translatedImage)
+    cv.waitKey()
+
+def scaling(image: cv.Mat, new_scale: float):
+    #get the height, width and channels of the image
+    height, width, channels = image.shape
+    newHeight = int((height * new_scale) / 100)
+    newWidth = int((width * new_scale) / 100)
+
+    scaledImage = cv.resize(image, (newHeight, newWidth), interpolation= cv.INTER_AREA)
+    cv.imshow('Scaled image', scaledImage)
+    cv.waitKey()
+
+def shearing(image: cv.Mat, x: int, y: int):
+    #get the height, width and channels of the image
+    height, width, channels = image.shape
+
+    #define the transformation Matrix
+    matrixX = np.float32([[1, x, 0],
+                         [0, 1, 0],
+                         [0, 0, 1]])
+    matrixY = np.float32([[1, 0, 0],
+                         [y, 1, 0],
+                         [0, 0, 1]])
+    shearedImageX = cv.warpPerspective(image, matrixX, (int(width * (1 + y)), int(height * (1 + x))));
+    cv.imshow('Sheared image on X axis', shearedImageX)
+    cv.waitKey()
+    shearedImageY = cv.warpPerspective(image, matrixY, (int(width * (1 + y)), int(height * (1 + x))));
+    cv.imshow('Sheared image on Y axis', shearedImageY)
+    cv.waitKey()
+
 image_path_folder = "/home/supreme/Master/Sem_1/FCV/Laborator/test_folder/854061.jpg"
 image = cv.imread(image_path_folder)
 
-adjustBrightness(image, 10)
-blurImage(image)
-edgeDetection(image)
+#adjustBrightness(image, 10)
+#blurImage(image)
+#edgeDetection(image)
+translation(image, 1, 1)
+scaling(image, 1.6)
+shearing(image, 0.3, 0.3)
